9706798db06f9a21e6d29c7b78aefe0904f01919
Kavisha <11203643@students.cs.ubc.ca>
2026-02-11 22:28:52 -0800

    created EntryRepository

---
+++ src/main/java/com/kavisha/learnjournal/repository/EntryRepository.java
@@ -0,0 +1,10 @@
+package com.kavisha.learnjournal.repository;
+
+
+import com.kavisha.learnjournal.model.Entry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface EntryRepository extends JpaRepository {
+    List<Entry> findByType(String type);
+}
